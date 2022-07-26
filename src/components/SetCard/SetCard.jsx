63c549a774c6080300a78b12ae41c7c56680dcdb
Endrews <elijah.smiael@gmail.com>
2022-07-26 17:52:57 -0500

    conditional fix for buttons on setCard

--- src/components/SetCard/SetCard.jsx
+++ src/components/SetCard/SetCard.jsx
@@ -3,23 +3,27 @@ import { Link } from 'react-router-dom'
 
 const Set = (props) => {
   const set = props.set
+  const isOwner = props.user?.profile === set.owner?._id
   return (
     <Link state={{set}} to={`/sets/${set._id}`}>
-    <div className={styles.card}>
+    <div className={styles.setCard}>
         <div className="card-body">
             <h2 className={styles.setHeader}>{set.title}</h2>
         </div>
+      { isOwner ?
+      <div className="card-body">
       <Link state={{set}} to={`/sets/${set._id}/edit`}>
       <button className="btn btn-secondary">Edit</button></Link>
-      {props.user?.profile === set.owner?._id &&
           <button 
           className="btn btn-danger"
             onClick={()=> props.handleDeleteSet(set._id)}
             >
             Delete
           </button>
-      
-    }
+        </div>
+        :
+        <h2>Yes</h2>
+      }
     </div>
     </Link>
   )
